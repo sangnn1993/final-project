4e9575f7c1a6244aa57fcdf147448775c683dfc1
sangnn1993 <sangnn1993@gmail.com>
2026-02-26 23:04:04 +0700

    test

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ export const Header = () => {
   return (
     <div className="space-y-2 text-center">
       <h1 className="text-5xl/normal font-bold text-transparent bg-primary bg-clip-text">
-        Danh Sách Công Việc
+        DEVOPS - FINAL PROJECT
       </h1>
       <p className="text-muted-foreground">
         Không có việc gì khó, chỉ sợ mình không làm ! 💪
