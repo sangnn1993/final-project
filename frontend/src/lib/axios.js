2c7ef91bbb7230ae3843a69835bdfd8053872df7
sangnn1993 <110016298+sangnn1993@users.noreply.github.com>
2026-02-27 17:50:30 +0700

    tst

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const isDev = import.meta.env.MODE === "development";
 
 const BASE_URL = isDev
   ? "http://localhost:5001/api"
-  : `${window.location.origin}/api`;   // tự động lấy dns
+  : `api.${window.location.origin}/api`;   // tự động lấy dns
 
 const api = axios.create({
   baseURL: BASE_URL,
