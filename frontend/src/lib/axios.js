dd49e9c0b8f6d876c24ab2fa28fbb29f974fa952
sangnn1993 <110016298+sangnn1993@users.noreply.github.com>
2026-02-27 17:53:12 +0700

    test

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const isDev = import.meta.env.MODE === "development";
 
 const BASE_URL = isDev
   ? "http://localhost:5001/api"
-  : `api.${window.location.origin}/api`;   // tự động lấy dns
+  : `http://api.fastinvest.cloud/api`;   // tự động lấy dns
 
 const api = axios.create({
   baseURL: BASE_URL,
